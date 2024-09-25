e781df1c9755c71576c06b368b234abbcdd0d5bf
Fernando <fernando.inversionmf@gmail.com>
2024-09-24 21:35:51 -0700

    add data router by React Router

---
+++ src/views/NewProduct.tsx
@@ -0,0 +1,53 @@
+import { Link, Form } from "react-router-dom";
+
+export async function action() {
+  console.log("from action");
+  return {};
+}
+export default function NewProduct() {
+  return (
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-4xl font-black text-slate-500">Register Product</h2>
+        <Link
+          to="/"
+          className="rounded-md bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500"
+        >
+          Back to Products
+        </Link>
+      </div>
+
+      <Form className="mt-10" method="POST">
+        <div className="mb-4">
+          <label className="text-gray-800" htmlFor="name">
+            Name Product:
+          </label>
+          <input
+            id="name"
+            type="text"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Name of Product. ej. TV, Laptop"
+            name="name"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="text-gray-800" htmlFor="price">
+            Price:
+          </label>
+          <input
+            id="price"
+            type="number"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Price of Product. ej. 300"
+            name="price"
+          />
+        </div>
+        <input
+          type="submit"
+          className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
+          value="Register Product"
+        />
+      </Form>
+    </>
+  );
+}
