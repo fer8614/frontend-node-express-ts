97bc8af4f22de507513b521bdf14837db84de61a
Fernando <fernando.inversionmf@gmail.com>
2024-09-26 19:00:02 -0700

    Retrieving the data entered in the form

--- src/views/NewProduct.tsx
+++ src/views/NewProduct.tsx
@@ -1,10 +1,25 @@
-import { Link, Form } from "react-router-dom";
+import { Link, Form, useActionData } from "react-router-dom";
 
-export async function action() {
-  console.log("from action");
+export async function action({ request }) {
+  const data = Object.fromEntries(await request.formData());
+
+  let error = "";
+  if (Object.values(data).includes("")) {
+    error = "All fields are required";
+  }
+  if (error.length) {
+    return error;
+  }
+
+  console.log(error);
+
   return {};
 }
 export default function NewProduct() {
+  const error = useActionData();
+
+  console.log(error);
+
   return (
     <>
       <div className="flex justify-between">
